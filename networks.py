1348faaf97baecc8b7cc9234351a3115a72ca910
jiaye-mao <610275388@qq.com>
2019-05-20 14:14:53 +0100

    add dropout options

--- networks.py
+++ networks.py
@@ -4,16 +4,16 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-# LeNet5
-class LeNet5(nn.Module):
+# LeNet5 with dropout
+class LeNet5_with_dropout(nn.Module):
     """
     The LeNet5 neural network architecture for a 3 channel 32x32
     input as expected from the CIFAR10 dataset
     """
 
-    def __init__(self, bias=True):
+    def __init__(self, bias=True, dropout=1):
         "setup the neural network"
-        
+
         # initalise
         super(LeNet5, self).__init__()
 
@@ -24,32 +24,111 @@ class LeNet5(nn.Module):
         # create each layer
         self.C1_layer = nn.Conv2d(1, 6, padding=2, kernel_size=kernal, stride=stride, bias=bias)
         self.C3_layer = nn.Conv2d(6, 16, padding=0, kernel_size=kernal, stride=stride, bias=bias)
-        
-        self.S2_layer = nn.MaxPool2d(kernel_size=2, stride=2)        
+
+        self.S2_layer = nn.MaxPool2d(kernel_size=2, stride=2)
         self.S4_layer = nn.MaxPool2d(kernel_size=2, stride=2)
-        
+
         self.F5_layer = nn.Linear(16*5*5, 120, bias=bias)
+
+        self.F55_layer = nn.dropout()
+
         self.F6_layer = nn.Linear(120,84, bias=bias)
-        
+        self.F7_layer = nn.dropout()
+
         self.output = nn.Linear(84, 10, bias=bias)
-        
+
         # create the activation function
         act = nn.ReLU()
-        
-        # create a list of fucntion order
-        self.layers = [self.C1_layer, act, 
-                       self.S2_layer, act, 
-                       self.C3_layer, act, 
-                       self.S4_layer, act, self.flatten, 
+
+        if(dropout == 1):
+            # create a list of fucntion order
+            self.layers = [self.C1_layer, act,
+                       self.S2_layer, act,
+                       self.C3_layer, act,
+                       self.S4_layer, act, self.flatten,
                        self.F5_layer, act,
+                       self.F55_layer, act,
                        self.F6_layer, act,
                        self.output]
-        
+        if(dropout == 2):
+            # create a list of fucntion order
+            self.layers = [self.C1_layer, act,
+                       self.S2_layer, act,
+                       self.C3_layer, act,
+                       self.S4_layer, act, self.flatten,
+                       self.F5_layer, act,
+                       self.F6_layer, act,
+                       self,F7_layer, act,
+                       self.output]
+        if (dropout == 3):
+            # create a list of fucntion order
+            self.layers = [self.C1_layer, act,
+                       self.S2_layer, act,
+                       self.C3_layer, act,
+                       self.S4_layer, act, self.flatten,
+                       self.F5_layer, act,
+                       self.F55_layer, act,
+                       self.F6_layer, act,
+                       self,F7_layer, act,
+                       self.output]
+
     def flatten(self, T):
         "flatten the image for the fully connected layers"
         return T.view(-1, 16*5*5)
-        
-        
+
+
+    def forward(self, x):
+        "Pass through the neural network"
+        for f in self.layers:
+            x = f(x)
+        return x
+
+
+# LeNet5
+class LeNet5(nn.Module):
+    """
+    The LeNet5 neural network architecture for a 3 channel 32x32
+    input as expected from the CIFAR10 dataset
+    """
+
+    def __init__(self, bias=True):
+        "setup the neural network"
+
+        # initalise
+        super(LeNet5, self).__init__()
+
+        # general params
+        kernal = 5
+        stride = 1
+
+        # create each layer
+        self.C1_layer = nn.Conv2d(1, 6, padding=2, kernel_size=kernal, stride=stride, bias=bias)
+        self.C3_layer = nn.Conv2d(6, 16, padding=0, kernel_size=kernal, stride=stride, bias=bias)
+
+        self.S2_layer = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.S4_layer = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.F5_layer = nn.Linear(16*5*5, 120, bias=bias)
+        self.F6_layer = nn.Linear(120,84, bias=bias)
+        self.output = nn.Linear(84, 10, bias=bias)
+
+        # create the activation function
+        act = nn.ReLU()
+
+        # create a list of fucntion order
+        self.layers = [self.C1_layer, act,
+                       self.S2_layer, act,
+                       self.C3_layer, act,
+                       self.S4_layer, act, self.flatten,
+                       self.F5_layer, act,
+                       self.F6_layer, act,
+                       self.output]
+
+    def flatten(self, T):
+        "flatten the image for the fully connected layers"
+        return T.view(-1, 16*5*5)
+
+
     def forward(self, x):
         "Pass through the neural network"
         for f in self.layers:
